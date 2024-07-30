e4d00f005dedb75e242c09e8f80a67020b8ff621
Olha <ovcharenko.olga.2014@gmail.com>
2024-07-30 21:43:49 +0300

    create section Intro

--- client/src/components/Pages/Home/Home.js
+++ client/src/components/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import Header from "../../Views/Header/Header";
+import Intro from "../../Views/Intro/Intro";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Intro />
     </div>
   )
 };
---
+++ client/src/components/Views/Intro/Intro.js
@@ -0,0 +1,11 @@
+import styles from "../Intro/Intro.module.scss";
+
+const Intro = () => (
+  <section className="Intro">
+    <div className={styles.container}>
+
+    </div>
+  </section>
+);
+
+export default Intro;
