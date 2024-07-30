57e23965b2be136a1e9441c3ff2d683b8fbd4b45
Olha <ovcharenko.olga.2014@gmail.com>
2024-07-30 22:13:47 +0300

    create section Footer

--- client/src/components/Pages/Home/Home.js
+++ client/src/components/Pages/Home/Home.js
@@ -1,3 +1,4 @@
+import Footer from "../../Views/Footer/Footer";
 import Header from "../../Views/Header/Header";
 import Intro from "../../Views/Intro/Intro";
 
@@ -6,6 +7,7 @@ const Home = () => {
     <div>
       <Header />
       <Intro />
+      <Footer />
     </div>
   )
 };
---
+++ client/src/components/Views/Footer/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <div className="text-center">
+       © 2024 OLHA OVCHARENKO
+      </div>
+    )
+  };
+    
+  export default Footer;
