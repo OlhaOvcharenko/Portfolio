fdda50df78a94b9a019edd166b7f0d0c7e6c2ff7
Olha <ovcharenko.olga.2014@gmail.com>
2024-08-04 14:30:51 +0300

    create Footer.module,scss and add styles for footer elements

--- client/src/components/Views/Footer/Footer.js
+++ client/src/components/Views/Footer/Footer.js
@@ -1,9 +1,26 @@
+import styles from "../Footer/Footer.module.scss"
+
 const Footer = () => {
-    return (
-      <div className="text-center">
-       © 2024 OLHA OVCHARENKO
+  return (
+    <div className={styles.footerContainer}>
+      <div className={styles.socialMedia}>
+        <div>
+          <a href="https://www.instagram.com/olga_ovcharenko_22_/" target="_blank">
+            <img width="30" height="30" src="https://img.icons8.com/3d-fluency/50/instagram-new.png" alt="instagram-new"/>
+          </a>
+          <a href="https://www.facebook.com/olga.ovcharenko.589/" target="_blank">
+            <img width="30" height="30" src="https://img.icons8.com/3d-fluency/94/facebook-circled.png" alt="facebook-circled"/>
+          </a>
+          <a href="www.linkedin.com/in/olha-ovcharenko-2633632b0" target="_blank">
+            <img width="30" height="30" src="https://img.icons8.com/3d-fluency/94/linkedin.png" alt="linkedin"/>
+          </a>
+        </div>
+        <div className={styles.footerText}>
+          <p> © 2024 OLHA OVCHARENKO </p>
+        </div>
       </div>
-    )
-  };
-    
-  export default Footer;
+    </div>
+  )
+};
+  
+export default Footer;
--- client/src/components/Views/Intro/Intro.js
+++ client/src/components/Views/Intro/Intro.js
@@ -5,7 +5,8 @@ const Intro = () => (
     <div className={styles.container}>
       <div className={styles.text}>
         <h1 className="text-center my-5">Hi, my name is Olha Ovcharenko!</h1>
-        <p>I am a Junior Full Stack Developer. Explore my projects.</p>
+        <p>I am a Junior Full Stack Developer.</p>
+        <p>Explore my projects.</p>
         <p>Feel free to contact me, share your thoughts and ideas at ovcharenko.olga.2014@gmail.com.</p>
       </div>
     </div>
