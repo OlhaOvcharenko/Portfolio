4ccf735b9569c9615b6afa5619f59e892c99f500
Olha <ovcharenko.olga.2014@gmail.com>
2024-08-04 15:23:04 +0300

    change footer.js, intro.module.scss, NavBar.module.scss

--- client/src/components/Views/Footer/Footer.js
+++ client/src/components/Views/Footer/Footer.js
@@ -4,15 +4,15 @@ const Footer = () => {
   return (
     <div className={styles.footerContainer}>
       <div className={styles.socialMedia}>
-        <div>
+        <div className={styles.mediaIcons}>
           <a href="https://www.instagram.com/olga_ovcharenko_22_/" target="_blank">
-            <img width="30" height="30" src="https://img.icons8.com/3d-fluency/50/instagram-new.png" alt="instagram-new"/>
+            <img width="40" height="40" src="https://img.icons8.com/ios-filled/50/instagram-new--v1.png" alt="instagram-new--v1"/>
           </a>
-          <a href="https://www.facebook.com/olga.ovcharenko.589/" target="_blank">
-            <img width="30" height="30" src="https://img.icons8.com/3d-fluency/94/facebook-circled.png" alt="facebook-circled"/>
+          <a href="https://www.facebook.com/olga.ovcharenko.589/" target="_blank" className={styles.fb}>
+            <img width="40" height="40" src="https://img.icons8.com/material/50/facebook-new.png" alt="facebook-new"/>
           </a>
-          <a href="www.linkedin.com/in/olha-ovcharenko-2633632b0" target="_blank">
-            <img width="30" height="30" src="https://img.icons8.com/3d-fluency/94/linkedin.png" alt="linkedin"/>
+          <a href="www.linkedin.com/in/olha-ovcharenko-2633632b0" target="_blank" className={styles.ln}>
+            <img width="40" height="40" src="https://img.icons8.com/ios-glyphs/50/linkedin.png" alt="linkedin"/>
           </a>
         </div>
         <div className={styles.footerText}>
