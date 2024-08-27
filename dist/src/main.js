cf64ea6a6bfaed3c999131d8fa9a6b8b79c8a207
Olha <ovcharenko.olga.2014@gmail.com>
2024-08-27 21:48:00 +0300

    create config.js, change projectsRedux.js

--- dist/src/main.js
+++ dist/src/main.js
@@ -5,9 +5,9 @@ const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3000);
+    await app.listen(8000);
     app.enableCors({
-        origin: 'http://localhost:8000',
+        origin: 'http://localhost:3000',
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
     });
