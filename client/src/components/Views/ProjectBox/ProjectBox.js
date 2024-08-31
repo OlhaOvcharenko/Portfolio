bab9156c84ae6dedd81399b4a59e6f8ce163b96a
Olha <ovcharenko.olga.2014@gmail.com>
2024-08-31 19:49:41 +0300

    change styles for projectBox by adding styles for title

--- client/src/components/Views/ProjectBox/ProjectBox.js
+++ client/src/components/Views/ProjectBox/ProjectBox.js
@@ -1,18 +1,19 @@
-import { Row, Card, Col } from "react-bootstrap";
 import { IMAGES_URL } from "../../../config";
 import styles from "../ProjectBox/ProjectBox.module.scss";
 
 const ProjectBox = ({ project }) => {
   return (
     <div className={styles.projectBox}>
-      <h1>{project.title}</h1>
+      <div className={styles.titleBox}>
+        <h2 className={styles.boxName}>{project.title}</h2>
+      </div>
       <img 
         src={`${IMAGES_URL}/${project.mainImg}`} 
         alt={project.title} 
         className={styles.mainImg} 
       />
     </div>
-  );
+    );
   };
   
   export default ProjectBox;
