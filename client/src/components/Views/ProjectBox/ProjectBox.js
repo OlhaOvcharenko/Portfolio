60c2eebb1354ce6048287c2f4a0c49cb12886118
Olha <ovcharenko.olga.2014@gmail.com>
2024-08-31 20:33:55 +0300

    change styles for projectBox by adding link

--- client/src/components/Views/ProjectBox/ProjectBox.js
+++ client/src/components/Views/ProjectBox/ProjectBox.js
@@ -1,4 +1,5 @@
 import { IMAGES_URL } from "../../../config";
+import { Link } from "react-router-dom";
 import styles from "../ProjectBox/ProjectBox.module.scss";
 
 const ProjectBox = ({ project }) => {
@@ -6,9 +7,11 @@ const ProjectBox = ({ project }) => {
     <div className={styles.projectBox}>
       <div className={styles.titleBox}>
         <h2 className={styles.boxName}>{project.title}</h2>
-        <div className={styles.btnBox}>
-          Read more ⟶
-        </div>
+        <Link style={{ textDecoration: 'none', color: 'inherit', padding:'0' }}>
+          <div className={styles.btnBox}>
+            Read more ⟶
+          </div>
+        </Link>
       </div>
       <img 
         src={`${IMAGES_URL}/${project.mainImg}`} 
