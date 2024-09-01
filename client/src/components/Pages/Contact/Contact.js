d986648004dfead5f09f9ef4e6dcbcc897903b7a
Olha <ovcharenko.olga.2014@gmail.com>
2024-09-01 21:18:24 +0300

    create Contact.module.scss and add styles for form

--- client/src/components/Pages/Contact/Contact.js
+++ client/src/components/Pages/Contact/Contact.js
@@ -1,6 +1,7 @@
 import ArrowLeft from "../../Common/ArrowLeft/ArrowLeft"
 import ArrowRight from "../../Common/ArrowRight/Arrow"
 import PageContainer from "../../Common/PageContainer/PageContainer"
+import styles from "../Contact/Contact.module.scss"
 
 
 const Contact = () => {
@@ -9,28 +10,35 @@ const Contact = () => {
       <ArrowRight to="/About" />
       <ArrowLeft to="/projects" />
       <h1 className="text-center">Contact me</h1>
-      <h3>Interested in collaborating on a project or discussing a potential opportunity? </h3>
-      <h3>Feel free to reach out to me using the form below.</h3>
+      <h3 className={styles.subTitle}>Interested in collaborating on a project or discussing a potential opportunity? </h3>
+      <h3 className={styles.subTitle}>Feel free to reach out to me using the form below.</h3>
       <form>
-        <div className="form-group">
-          <label htmlFor="name">Your Name</label>
-          <input type="text" id="name" name="name"required />
+        <div className={styles.contactBox}>
+          <div className={styles.formGroup}>
+            <label htmlFor="name">Your Name</label>
+            <input type="text" id="name" name="name"required />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="email">Your Email</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="text">Message</label>
+            <input type="text" id="text" name="text" required />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="text">Subject</label>
+            <input type="text" id="text" name="text" required />
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-        <div className="form-group">
-          <label htmlFor="text">Message</label>
-          <input type="text" id="text" name="text" required />
-        </div>
-        <div className="form-group">
-          <label htmlFor="text">Subject</label>
-          <input type="text" id="text" name="text" required />
-        </div>
-        
+
+          <div className={styles.textBox}>
+            <label htmlFor="message">Your Message</label>
+            <textarea id="message" name="message" rows="4" required></textarea>
+          </div>
+
         <button type="submit" className="btn btn-primary">Send message</button>
-    </form>
+      </form>
   </PageContainer>
   )
 }
