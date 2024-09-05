4c9e2e9a6222987fd4901fe40d331cceee3c736d
Olha <ovcharenko.olga.2014@gmail.com>
2024-09-05 21:58:40 +0300

    change About.js by adding one more paragrph[C

--- client/src/components/Pages/About/About.js
+++ client/src/components/Pages/About/About.js
@@ -13,14 +13,19 @@ const About = () => {
         <ArrowRight to="/contact" />
         <ArrowLeft to="/projects" />
         <div className={styles.aboutBox}>
-            <div  className={styles.infoBox}>
+          <div  className={styles.infoBox}>
+            <div  className={styles.textBox}>
                 <p>I'm a Full-Stack Developer with a passion for creating innovative and user-friendly web applications.</p>
                 <p>With a focus on front-end and back-end development, I strive to deliver high-quality solutions that meet the unique needs of each project.</p> 
                 <p>This is a space where I showcase my work and share my journey in the world of web development.</p>
             </div>
-            <div className={styles.photoBox}>
-              <img src={profileImageUrl} alt="Brand Logo" />
+            <div>
+                <p>[...click here to check CV]</p>
             </div>
+          </div>
+          <div className={styles.photoBox}>
+            <img src={profileImageUrl} alt="Brand Logo" />
+          </div>
         </div>
       </PageContainer>
     );
