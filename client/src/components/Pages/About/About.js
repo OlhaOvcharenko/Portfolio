0745d39edaa1e7eeb09c2414c25b1fcb10fc52e6
Olha <ovcharenko.olga.2014@gmail.com>
2024-09-05 21:46:17 +0300

    change styles for About.js

--- client/src/components/Pages/About/About.js
+++ client/src/components/Pages/About/About.js
@@ -2,21 +2,23 @@ import PageContainer from "../../Common/PageContainer/PageContainer";
 import ArrowRight from "../../Common/ArrowRight/Arrow";
 import ArrowLeft from "../../Common/ArrowLeft/ArrowLeft";
 import { IMAGES_URL } from "../../../config";
+import styles from "../About/About.module.scss";
+
 
 const About = () => {
     const profileImageUrl = `${IMAGES_URL}/profile_photo.jpg`;
     return (
       <PageContainer>
-        <h1>About</h1>
+        <h1>A few words about me...</h1>
         <ArrowRight to="/contact" />
         <ArrowLeft to="/projects" />
-        <div>
-            <div>
+        <div className={styles.aboutBox}>
+            <div  className={styles.infoBox}>
                 <p>I'm a Full-Stack Developer with a passion for creating innovative and user-friendly web applications.</p>
                 <p>With a focus on front-end and back-end development, I strive to deliver high-quality solutions that meet the unique needs of each project.</p> 
                 <p>This is a space where I showcase my work and share my journey in the world of web development.</p>
             </div>
-            <div>
+            <div className={styles.photoBox}>
               <img src={profileImageUrl} alt="Brand Logo" />
             </div>
         </div>
