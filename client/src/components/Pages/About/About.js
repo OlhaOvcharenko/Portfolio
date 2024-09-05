77d17227885a179d7486ddc50c843f486314beb6
Olha <ovcharenko.olga.2014@gmail.com>
2024-09-05 21:22:31 +0300

    change About.js by adding info and photo

--- client/src/components/Pages/About/About.js
+++ client/src/components/Pages/About/About.js
@@ -1,13 +1,25 @@
 import PageContainer from "../../Common/PageContainer/PageContainer";
 import ArrowRight from "../../Common/ArrowRight/Arrow";
 import ArrowLeft from "../../Common/ArrowLeft/ArrowLeft";
+import { IMAGES_URL } from "../../../config";
 
 const About = () => {
+    const profileImageUrl = `${IMAGES_URL}/profile_photo.jpg`;
     return (
       <PageContainer>
         <h1>About</h1>
         <ArrowRight to="/contact" />
         <ArrowLeft to="/projects" />
+        <div>
+            <div>
+                <p>I'm a Full-Stack Developer with a passion for creating innovative and user-friendly web applications.</p>
+                <p>With a focus on front-end and back-end development, I strive to deliver high-quality solutions that meet the unique needs of each project.</p> 
+                <p>This is a space where I showcase my work and share my journey in the world of web development.</p>
+            </div>
+            <div>
+              <img src={profileImageUrl} alt="Brand Logo" />
+            </div>
+        </div>
       </PageContainer>
     );
   };
