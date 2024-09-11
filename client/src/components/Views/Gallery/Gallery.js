24a053ec3b4108626f1b46bc3df3e2c01c0a5dff
Olha <ovcharenko.olga.2014@gmail.com>
2024-09-11 21:48:02 +0300

    change styles for Gallery by adding new styles for images

--- client/src/components/Views/Gallery/Gallery.js
+++ client/src/components/Views/Gallery/Gallery.js
@@ -1,18 +1,42 @@
+import React from 'react';
+import { useState } from 'react';
 import { IMAGES_URL } from '../../../config';
+
 import styles from '../Gallery/Gallery.module.scss';
 
+
 const Gallery = ({ galleryImages }) => {
+  const [selectedImage, setSelectedImage] = useState(`${IMAGES_URL}/${galleryImages[0]}`);
+
+  const handleClickImage = (image) => {
+    setSelectedImage(`${IMAGES_URL}/${image}`);
+  };
+
   return (
-    <div className={`row ${styles.slider}`}>
-      {galleryImages.map((image, i) => (
-        <div key={i} className={`${styles.item}`}>
-          <img
-            src={`${IMAGES_URL}/${image}`}
-          />
-        </div>
-      ))}
+    <div className={styles.galleryWrapper}>
+      <div className={styles.selectedImageContainer}>
+        <img
+          src={selectedImage}
+          alt="Selected"
+          className={styles.selectedImage}
+        />
+      </div>
+
+      <div className={styles.slider}>
+        {galleryImages.map((image, i) => (
+          <div key={i} className={styles.item}>
+            <img
+              src={`${IMAGES_URL}/${image}`}
+              alt={`galleryImg ${i}`}
+              className={styles.galleryImage}
+              onClick={() => handleClickImage(image)}
+              style={{ cursor: 'pointer' }}
+            />
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 };
 
 export default Gallery;
