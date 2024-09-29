a98c9b40fb8ddbda8a71b671c02c397b187b3a07
Olha <ovcharenko.olga.2014@gmail.com>
2024-09-29 21:38:49 +0300

    change Projects.js by replacing link

--- client/src/components/Pages/Projects/Projects.js
+++ client/src/components/Pages/Projects/Projects.js
@@ -10,7 +10,7 @@ const Projects = () => {
   const allProjects = useSelector(getAllProjects);
 
   return (
-    <PageContainer arrowContainerClass={styles.arrows} toNext={"/contact"} toPrevious={"/"}>
+    <PageContainer arrowContainerClass={styles.arrows} toNext={"/about"} toPrevious={"/"}>
       <h1 className="text-center"><i>All Projects</i></h1>
         <Container>
           <Row lg={2}className=" g-4 py-3">
