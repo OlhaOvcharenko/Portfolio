ab2d2d7ce613030649d27cabdf7b0028b6501a63
Olha <ovcharenko.olga.2014@gmail.com>
2024-10-01 19:30:20 +0200

    create email module/controller/service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectsModule } from './projects/projects.module';
 import { ContactModule } from './contact/contact.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [ProjectsModule, ContactModule],
+  imports: [ProjectsModule, ContactModule, EmailModule],
   controllers: [AppController],
   providers: [AppService],
 })
